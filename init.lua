22ff291c8a6eb30397a33e14d792542284a286a2
Yan Davidovich <yan_davidovich@epam.com>
2015-10-07 14:34:43 +0300

    fix file names

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 wifi.setmode(wifi.STATION)
 wifi.sta.config("SSID", "PASSWORD")
 
-dofile('telnet.lc')
+dofile('telnet.lua')
 
-tmr.alarm(1, 8000, 0, function() print(wifi.sta.getip()) dofile('milight.lc') end )
+tmr.alarm(1, 8000, 0, function() print(wifi.sta.getip()) dofile('milight.lua') end )
